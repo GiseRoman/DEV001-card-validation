0156fd0f0280aece2461115547164d7d491932e2
Gise <gisenoctis@gmail.com>
2022-10-24 18:30:27 -0500

    card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("donar").focus();
+
+const btnCard = document.querySelector('[data-form-btn]');
+// verificar la validez de número de tarjeta
+const valid = (evento) => {
+    evento.preventDefault();
+    const val = document.querySelector('[data-form-input]');
+    const n = val.value;
+    let cc = n;
+    const numb = validator.isValid(n);
+    const validNumb = validator.validation(numb);
+    if (validNumb === true) {
+        mostrar("cardValid");
+    }else{
+        mostrar("cardInvalid");
+    }
+    // const resultVal = document.querySelector("[data-result]");
+    // const resultP = document.createElement("li");
+    // resultP.classList.add("avisResult");
+    val.value = validator.maskify(cc);
+    // const result = `<div><p>${validNumb}</p></div>`;
+    // resultP.innerHTML = result;
+    // resultVal.appendChild(resultP);
+};
+
+btnCard.addEventListener("click", valid);
+
+const inputCard = document.querySelector('[data-form-input]');
+// convertir en # los valores conforme se escriben en el input
+const ocultNumb = () =>{
+    const num = document.querySelector('[data-form-input]');
+    let cc = num.value;
+    num.value = validator.maskify(cc);
+};
+
+inputCard.addEventListener("keypress", ocultNumb);
+
+function mostrar(section) {
+    const most = document.getElementsByClassName("oculto");
+    for (let i = 0, ocul =most.length; i < ocul; i++){
+        most[i].getElementsByClassName.display = "none";
+    }
+    document.getElementById(section).style.display = "block";
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+// ocultar los valores ingresados, excepto los ultimos 4
+    maskify (cc){
+        cc = cc.split("");
+        for(let i = 0; i < cc.length - 4; i++){
+            cc[i] = "#";
+        }
+        cc = cc.join("");
+        return cc;
+    },
+// validador de tarjeta
+    isValid (n){
+        let sum = 0;
+        while (n > 0){
+          let a = n % 10;
+          n = Math.floor(n / 10);
+          let b = (n % 10) * 2;
+          n = Math.floor(n / 10);
+          if (b > 9){
+            b -= 9;
+          }
+          sum += a + b;
+        }
+        const valid = sum % 10 == 0;
+        return valid;
+      },
+// Informar al usuario si la tarjeta es valida
+      validation (n){
+        if (n === true){
+          return "Tu tarjeta es válida";
+        }else{
+          return "Tu tarjeta es invalida";
+        }
+      }
 };
-
 export default validator;
